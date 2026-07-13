d4510f6aa4b227f59095f8717a155549ff3ec3a6
Altana <altn.grishkina@capgemini.com>
2026-07-13 10:44:31 +0200

    145. More closure examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -317,7 +317,7 @@ poll.displayResults.call({answers: [1, 5, 3, 9, 6]}); */
 
 // 143. Immediately Invoked Function Expressions (IIFE)
 
-const runOnce = function(){
+/* const runOnce = function(){
     console.log('This will never run again');
 }
 
@@ -338,3 +338,77 @@ runOnce();
 }
 console.log(isPrivate); // ReferenceError: isPrivate is not defined
 console.log(notPrivate); // 46
+ */
+
+
+
+
+
+
+
+
+// 144. Closures
+
+/* const secureBooking = function(){
+    let passengerCount = 0;
+
+    return function(){
+        passengerCount++;
+        console.log(`${passengerCount} passengers`);
+    }
+}
+
+const booker = secureBooking();
+booker();
+booker();
+booker();
+
+console.dir(booker); // shows the closure in the console
+ */
+
+
+
+
+
+// 145. More closure Examples
+
+// Example 1
+let f;
+const g = function(){
+    const a = 23;
+    f = function(){
+        console.log(a*2);
+        
+    }
+}
+
+const h = function(){
+    const b = 777;
+    f = function(){
+        console.log(b*2);
+        
+    }
+}
+
+g();
+f();
+console.dir(f);
+
+// Re-assign f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function(n, wait){
+    const perGroup = n/3;
+    setTimeout(function(){
+        console.log(`We are now boarding ${n} passenger`);
+        console.log(`There are 3 groups, each with ${perGroup} passengers`);
+    }, wait * 1000);
+
+    console.log(`Will start boarding in ${wait} seconds`);
+}
+
+const perGroup = 1000;
+boardPassengers(180, 3);
