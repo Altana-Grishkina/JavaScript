dbff7d8e359a1ad04312bb45707adc7a4e618f01
Altana <altn.grishkina@capgemini.com>
2026-07-14 17:51:34 +0200

    150. The new at method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -76,7 +76,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
 
 //SLICE
-let arr = ['a', 'b', 'c', 'd', 'e'];
+/* let arr = ['a', 'b', 'c', 'd', 'e'];
 
 console.log(arr.slice(2));
 console.log(arr.slice(2, 4));
@@ -108,7 +108,27 @@ console.log(letters);
 console.log([...arr,...arr2]);
 
 //JOIN
-console.log(letters.join(' - '));
+console.log(letters.join(' - ')); */
+
+
+
+
+
+// 150. The new at method
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+//getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-2));
+
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
+
+
 
 
 
