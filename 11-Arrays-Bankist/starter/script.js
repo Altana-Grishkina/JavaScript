d59584982d9cba52adea8ad51e5b0704f4ed71f9
Altana <altn.grishkina@capgemini.com>
2026-07-15 11:20:42 +0200

    151. Looping arrays: forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
 
 /////////////////////////////////////////////////
 
@@ -116,7 +116,7 @@ console.log(letters.join(' - ')); */
 
 // 150. The new at method
 
-const arr = [23, 11, 64];
+/* const arr = [23, 11, 64];
 console.log(arr[0]);
 console.log(arr.at(0));
 
@@ -126,7 +126,7 @@ console.log(arr.slice(-1)[0]);
 console.log(arr.at(-2));
 
 console.log('jonas'.at(0));
-console.log('jonas'.at(-1));
+console.log('jonas'.at(-1)); */
 
 
 
@@ -134,3 +134,31 @@ console.log('jonas'.at(-1));
 
 
 
+// 151. Looping arrays: forEach
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for(const movement of movements){
+for(const [i, movement] of movements.entries()){
+  if(movement > 0){
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+// forEach
+
+console.log('---- FOREACH -----');
+
+movements.forEach(function(mov, i, arr){
+   if(mov > 0){
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+})
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
