c923fd496ef55dcdeb7381c3da38e97c04af3689
Altana <altn.grishkina@capgemini.com>
2026-07-29 18:04:05 +0200

    158. Computing Usernames

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -231,22 +231,155 @@ let dogsKate = [4, 1, 15, 7]; */
 
 
 
-function checkDogs(dogsJulia, dogsKate){
-let copyDogsJulia = [...dogsJulia];
-const justDogsJulia = copyDogsJulia.slice(1, -2); 
- const together = [...justDogsJulia, ...dogsKate]
+// function checkDogs(dogsJulia, dogsKate){
+// let copyDogsJulia = [...dogsJulia];
+// const justDogsJulia = copyDogsJulia.slice(1, -2); 
+//  const together = [...justDogsJulia, ...dogsKate]
 
-together.forEach(function(i, value){
-      if(value > 3){
-        console.log(`Dog number ${i} is an adult, and is ${value} years old `);
-      } else {
-        console.log(`Dog number ${i} is still a puppy`);
-      }
- }); 
+// together.forEach(function(i, value){
+//       if(value > 3){
+//         console.log(`Dog number ${i} is an adult, and is ${value} years old `);
+//       } else {
+//         console.log(`Dog number ${i} is still a puppy`);
+//       }
+//  }); 
  
-}
+// }
 
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 // TEST DATA 1: Julia's data [3, 5, 2, 12, 3], Kate's data [4, 1, 15, 7]
 
 // TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+
+
+
+
+
+
+// 157. The map Method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+// const movementsUsd = movements.map(function(mov){
+//   return mov * eurToUsd;
+// })
+
+const movementsUsd = movements.map((mov) => mov * eurToUsd);
+console.log(movements);
+console.log(movementsUsd);
+
+const movenentsUSDfor = [];
+for (const mov of movements) movenentsUSDfor.push(mov * eurToUsd);
+console.log(movenentsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) => 
+  `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`
+);
+
+console.log(movementsDescriptions);
+
+
+
+
+
+
+
+// 158. Computing Usernames
+// BANKIST APP
+
+/////////////////////////////////////////////////
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+  type: 'premium',
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+  type: 'standard',
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+  type: 'premium',
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+  type: 'basic',
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// // Elements
+// const labelWelcome = document.querySelector('.welcome');
+// const labelDate = document.querySelector('.date');
+// const labelBalance = document.querySelector('.balance__value');
+// const labelSumIn = document.querySelector('.summary__value--in');
+// const labelSumOut = document.querySelector('.summary__value--out');
+// const labelSumInterest = document.querySelector('.summary__value--interest');
+// const labelTimer = document.querySelector('.timer');
+
+// const containerApp = document.querySelector('.app');
+// const containerMovements = document.querySelector('.movements');
+
+// const btnLogin = document.querySelector('.login__btn');
+// const btnTransfer = document.querySelector('.form__btn--transfer');
+// const btnLoan = document.querySelector('.form__btn--loan');
+// const btnClose = document.querySelector('.form__btn--close');
+// const btnSort = document.querySelector('.btn--sort');
+
+// const inputLoginUsername = document.querySelector('.login__input--user');
+// const inputLoginPin = document.querySelector('.login__input--pin');
+// const inputTransferTo = document.querySelector('.form__input--to');
+// const inputTransferAmount = document.querySelector('.form__input--amount');
+// const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+// const inputCloseUsername = document.querySelector('.form__input--user');
+// const inputClosePin = document.querySelector('.form__input--pin');
+
+// const displayMovements = function(movements){
+//   containerMovements.innerHTML = '';
+//   //.textContent = 0
+//   movements.forEach(function(mov, i){
+//     const type = mov > 0 ? 'deposit' : 'withdrawal';
+//     const html = `
+//         <div class="movements__row">
+//           <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+//           <div class="movements__value">${mov}</div>
+//         </div>`
+//     containerMovements.insertAdjacentHTML('afterbegin', html);
+//   });
+  
+// };
+// displayMovements(account1.movements); 
+
+const createUsernames = function(accs){
+  accs.forEach(function(acc){
+    acc.username = acc.owner
+                .toLowerCase()
+                .split(' ')
+                .map(name => name[0])
+                .join('');
+  })
+};
+
+createUsernames(accounts);
+console.log(accounts);
+
+
+
