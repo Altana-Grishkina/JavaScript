b5bb72f2b02cc123db37f6f8b27c06ceaa3c5090
Altana <altn.grishkina@capgemini.com>
2026-08-14 00:29:40 +0200

    Challenge 3

--- 11-Arrays-Bankist/starter/challenge_2.js
+++ 11-Arrays-Bankist/starter/challenge_2.js
@@ -1,11 +1,17 @@
 
 
-const calcAverageHumanAge = function(ages) {
-  const humanAges = ages.map(age => age <= 2 ? age * 2 : 16 + age * 4);
-  const adults = humanAges.filter(age => age >= 18);
-     return adults.reduce((sum, age) => sum + age, 0) / adults.length;
-     // consta average = adults.reduce((acc, age, i, arr) => acc + age / arr.length, 0);
-}
+// const calcAverageHumanAge = function(ages) {
+//   const humanAges = ages.map(age => age <= 2 ? age * 2 : 16 + age * 4).filter(age => age >= 18);
+//   return humanAges.reduce((sum, age) => sum + age, 0) / humanAges.length;
+// }
+
+// Challenge 3
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((sum, age, _, arr) => sum + age / arr.length, 0);
+
 
 const averageAge1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 console.log(averageAge1);
